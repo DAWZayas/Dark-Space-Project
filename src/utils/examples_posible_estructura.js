7cb01d45a2d45512252f1f7e88d340ab01ad2711
raulgarciade <garcia.de.la.fuente.raul@gmail.com>
2015-11-12 12:21:23 +0200

    batalla sin return

--- src/utils/examples_posible_estructura.js
+++ src/utils/examples_posible_estructura.js
@@ -36,13 +36,12 @@ shield "escudo", no deja queun golpe inferior al shield power le haga daño a la
   precision, probabilidad de hacer el doble de daño por disparo
 */
 var playerFleet = [
-  {
-    id      : 1,
+  
+    {
     type    :'cruise',
-    shield  : 'yes',
     shield_power : 150,
     alive   : 3000,
-    damege  : 150,
+    damage  : 150,
     shots   : 8,
     precision : 20
 
@@ -51,10 +50,9 @@ var playerFleet = [
     {
     id      : 2,
     type    :'cruise',
-    shield  : 'yes',
     shield_power : 150,
     alive   : 3000,
-    damege  : 150,
+    damage  : 150,
     shots   : 8,
     precision : 20
 
@@ -63,7 +61,6 @@ var playerFleet = [
   {
     id      : 3,
     type    :'destroyer',
-    shield  : 'yes',
     shield_power : 100,
     alive   : 1000,
     shots   : 4,
@@ -73,7 +70,6 @@ var playerFleet = [
   {
     id: 4,
     type    :'fighter',
-    shield  : 'no',
     shield_power : 0,
     alive   : 100,
     shots   : 2,
@@ -83,7 +79,6 @@ var playerFleet = [
     {
     id: 5,
     type    :'fighter',
-    shield  : 'no',
     shield_power : 0,
     alive   : 100,
     shots   : 2,
@@ -95,10 +90,9 @@ var dummyFleet = [
   {
     id      : 1,
     type    :'cruise',
-    shield  : 'yes',
     shield_power : 150,
     alive   : 3000,
-    damege  : 150,
+    damage  : 150,
     shots   : 8,
     precision : 20
 
@@ -107,10 +101,9 @@ var dummyFleet = [
     {
     id      : 2,
     type    :'cruise',
-    shield  : 'yes',
     shield_power : 150,
     alive   : 3000,
-    damege  : 150,
+    damage  : 150,
     shots   : 8,
     precision : 20
 
@@ -119,10 +112,9 @@ var dummyFleet = [
   {
     id      : 3,
     type    :'cruise',
-    shield  : 'yes',
     shield_power : 150,
     alive   : 3000,
-    damege  : 150,
+    damage  : 150,
     shots   : 8,
     precision : 20
 
@@ -131,7 +123,6 @@ var dummyFleet = [
   {
     id      : 4,
     type    :'destroyer',
-    shield  : 'yes',
     shield_power : 100,
     alive   : 1000,
     shots   : 4,
@@ -141,7 +132,6 @@ var dummyFleet = [
   {
     id:5,
     type    :'fighter',
-    shield  : 'no',
     shield_power : 0,
     alive   : 100,
     shots   : 2,
@@ -151,7 +141,6 @@ var dummyFleet = [
     {
     id: 6,
     type    :'fighter',
-    shield  : 'no',
     shield_power : 0,
     alive   : 100,
     shots   : 2,
@@ -201,13 +190,46 @@ var playerDefenses = [
 //1º funcion random, si es par empieza el jugador, si es b empieza la maquina
 
 
+
+
+//turno
+function Battle(playerFleet,dummyFleet){
+    var att = initial() 
+ 
+ if (att == "dummyFleet"){
+      attacker = playerFleet;
+      defense = dummyFleet;
+      
+    }else if(att == "playerFleet" ){
+      attacker = dummyFleet;
+      defense = playerFleet;
+    }
+
+  var turn=1;
+
+    while (a <= 8 ){
+      var aux_at=attacker;
+      var aux_def=defense;
+
+      defense=aux_at;
+      attacker=aux_def;
+
+      defense=round(attacker,defense);
+
+      turn++;
+    }
+
+    //final batalla    
+}
+    
+
 function initial(){
               var rand = getRandomInt(1,100);
               var decision = 0;
               if ( rand > 50){
-                  decision = 1;
+                  decision = "playerFleet";
               }else if(rand < 50 ){
-                  decision = 2;
+                  decision = "dummyFleet";
               }else if(rand == 50){
 
                 decision = initial();
@@ -215,30 +237,52 @@ function initial(){
             return decision;
           }
 
+function round(attacker,defense){
+    while (var i = 0; i <= attacker.length ; i++){
+          if (attacker[i].alive !== 0){
+              var damage     = attacker[i].damage;
+              var precision  = attacker[i].precision;
+              
+              //es critic?
+              critical_attack(getRandomInt(1,100),precision)
+              if (critical_attack == "true"){
+                damage=damage*2;
+              }
+              //nave objetivo
+              nav_target=target(defense);
+              defense=attac(damage,defense,nav_target);
 
-// devuelve 1 o 2 dependiendo quien inicia la atalla
-console.log(var attacker = initial() )
-
-//turno
-function Battle(attacker){
- if (attacker == 1){
-      var Turno = playerFleet;
-    }else if(attacker == 2){
-      var Turno = dummyFleet;
+          }
     }
-
-
-    
-}
-    
-
-
-//2º ataque (por cada unidad, y cada disparo de la misma)
-while (var i = 0; i <= turno.length ; i++){
-    Turno[i][]
-
+    return defense;
 }
 
+function attac(damage,defense,nav_target){
+            shield  =defense[nav_target].shield_power;
+            alive   =defense[nav_target].alive;
+            if (damage > shield){
+
+              damage=damage-shield;
+              if (damage > 0){
+                  alive=alive-damage;
+              }
+            }
+            if (alive < 0){
+                alive = 0;
+            }
+        defense=actu_nav(nav_target,defense,alive);
+
+  return defense;
+}
+function actu_nav(nav_target,defense,alive){
+  defense[nav_target].life=alive;
+  return defense;
+}
+function target(defense){
+  nav_target=getRandomInt(0,playerFleet.length)-1);
+
+return v_target;
+}
 
   // 2.1  calcular un numero de 1 al 100 si sale un numero del 1 al numero de la variable precision es critico
 
@@ -258,7 +302,7 @@ while (var i = 0; i <= turno.length ; i++){
           }
 
             function getRandomInt(min, max) {
-                return Math.floor((Math.random() * 100) + 1);
+                return Math.floor((Math.random() * max) + 1);
             }
 
             function critical_attack(crit,precision){
