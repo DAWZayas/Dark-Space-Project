daef3d4e1f49b1089dc4b3512450072fb437c9ba
raulgarciade <garcia.de.la.fuente.raul@gmail.com>
2015-11-12 19:27:14 +0200

    arreglos

--- src/utils/examples_posible_estructura.js
+++ src/utils/examples_posible_estructura.js
@@ -1,37 +1,5 @@
-/*var campaign = [
-  {
-    id: 1,
-    title: 'Battle of Chandrila',
-    status: 'success',
-    percentage: 100
-  },
-  {
-    id: 2,
-    title: 'Battle of Yavin 4',
-    status: 'success',
-    percentage: 55
-  },
-  {
-    id: 3,
-    title: 'Attack on Coruscant',
-    status: 'fail',
-    percentage: 30
-  },
-  {
-    id: 4,
-    title: 'First battle of Korriban',
-    status: 'fail',
-    percentage: 0
-  },
-  {
-    id: 5,
-    title: 'Second battle of Korriban',
-    status: 'fail',
-    percentage: 0
-  }
-];
 
-
+/*
 shield "escudo", no deja queun golpe inferior al shield power le haga daño a la nave 
   precision, probabilidad de hacer el doble de daño por disparo
 */
@@ -148,52 +116,11 @@ var dummyFleet = [
     precision : 15
   }
 ];
-/*
-var playerDefenses = [
-    {
-    id      : 1,
-    type    :'dome',
-    shield  : 'yes',
-    shield_power : 1000,
-    distance:"",
-    alive   : 1
-    shots   : 0,
-    damage  : 0,
-  },
-  {
-    id      : 2,
-    type    :'Gauss canyon ',
-    shield  : 'no',
-    shield_power : 0,
-    alive   : 160,
-    distance: "long",
-    shots   : 3,
-    damage  : 250,
-    precision : 45
-  },
 
-  {
-    id      : 3,
-    type    :'missile launches',
-    shield  : 'no',
-    shield_power : 0,
-    alive   : 180,
-    distance :"short",
-    shots   : 5,
-    damage  : 200,
-    precision : 15
-  }
-];
-
-\\\\\\batalla espacial//////
-*/
-//1º funcion random, si es par empieza el jugador, si es b empieza la maquina
-
-
-
-
+//\\\\\\batalla espacial//////
 //turno
-function Battle(playerFleet,dummyFleet){
+
+function Battle(playerFleet,dummyFleet,turns){
     var att = initial() 
  
  if (att == "dummyFleet"){
@@ -207,7 +134,7 @@ function Battle(playerFleet,dummyFleet){
 
   var turn=1;
 
-    while (a <= 8 ){
+    while (turn <= turns ){
       var aux_at=attacker;
       var aux_def=defense;
 
@@ -221,8 +148,6 @@ function Battle(playerFleet,dummyFleet){
 
     //final batalla    
 }
-    
-
 function initial(){
               var rand = getRandomInt(1,100);
               var decision = 0;
@@ -236,21 +161,19 @@ function initial(){
               }
             return decision;
           }
-
 function round(attacker,defense){
     while (var i = 0; i <= attacker.length ; i++){
           if (attacker[i].alive !== 0){
-              var damage     = attacker[i].damage;
-              var precision  = attacker[i].precision;
-              
-              //es critic?
-              critical_attack(getRandomInt(1,100),precision)
-              if (critical_attack == "true"){
-                damage=damage*2;
+                var damage     = attacker[i].damage;
+                var precision  = attacker[i].precision;
+                
+                //es critic?
+                critical_attack(getRandomInt(1,100),precision)
+                if (critical_attack == "true"){
+                  damage=damage*2;
               }
               //nave objetivo
-              nav_target=target(defense);
-              defense=attac(damage,defense,nav_target);
+              defense=attac(damage,defense,target(defense));
 
           }
     }
@@ -263,9 +186,9 @@ function attac(damage,defense,nav_target){
             if (damage > shield){
 
               damage=damage-shield;
-              if (damage > 0){
-                  alive=alive-damage;
-              }
+              
+              alive=alive-damage;
+              
             }
             if (alive < 0){
                 alive = 0;
@@ -275,65 +198,39 @@ function attac(damage,defense,nav_target){
   return defense;
 }
 function actu_nav(nav_target,defense,alive){
-  defense[nav_target].life=alive;
+  defense[nav_target].alive=alive;
   return defense;
 }
 function target(defense){
   nav_target=getRandomInt(0,playerFleet.length)-1);
 
-return v_target;
+  return nav_target;
 }
 
-  // 2.1  calcular un numero de 1 al 100 si sale un numero del 1 al numero de la variable precision es critico
+function initial(){
+    var rand = getRandomInt(1,100);
+    var decision = 0;
+    if ( rand > 50){
+        decision = 1;
+    }else if(rand < 50 ){
+        decision = 2;
+    }else if(rand =50){
 
-          // var precision = 40;
-          function initial(){
-              var rand = getRandomInt(1,100);
-              var decision = 0;
-              if ( rand > 50){
-                  decision = 1;
-              }else if(rand < 50 ){
-                  decision = 2;
-              }else if(rand =50){
+      desision = initial();
+    }
+  return desision;
+}
 
-                desision = initial();
-              }
-            return desision;
-          }
+  function getRandomInt(min, max) {
+      return Math.floor((Math.random() * max) + 1);
+  }
 
-            function getRandomInt(min, max) {
-                return Math.floor((Math.random() * max) + 1);
-            }
-
-            function critical_attack(crit,precision){
-             
-             console.log(crit);
-             if (crit <= precision){
-              return true;
-             }else{
-               return false;
-               }
-            }
-
-            console.log(critical_attack(getRandomInt(1,100),precision));
-
-
-   // 2.1 ataca a una unidad del array enemigo de naves
-
-            console.log(getRandomInt(1,array.length));
- //             le hara a la unidad objetivo el daño de la atacante si critical_attack sale false o el doble si sale true
- //             (el daño se restara a shield power y despues a la vida de la unidad atacada).
-//3º si la vida todal de la la vida de la flota se reduce a un 30% del original 
-
-
-
-
-
-
-/*
-\\\\\\batalla tierra//////
-
-
-
-
-*/
+  function critical_attack(crit,precision){
+   
+   console.log(crit);
+   if (crit <= precision){
+    return true;
+   }else{
+     return false;
+     }
+  }
