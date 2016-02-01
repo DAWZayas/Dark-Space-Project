38539dac734c6a996811b998d29d1d6279b56e2e
Alejandro Martin Belchi <letzgar@gmail.com>
2016-02-01 11:47:10 +0100

    signin fixed

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -34,9 +34,9 @@ export default class SignIn extends Component {
           <h4 className="modal-title">Sign in Dialog</h4>
         </div>
         <div className="modal-body">
-          <button className="btn" type="button" onClick={ () => this.handlesignInWithGithubClick()}>GitHub</button>
-          <button className="btn" type="button" onClick={ () => this.handlesignInWithTwitterClick()}>Twitter</button>
-          <button className="btn" type="button" onClick={ () => this.handlesignInWithGoogleClick()}>Google</button>
+          <button className="text btn" type="button" onClick={ () => this.handlesignInWithGithubClick()}>GitHub</button>
+          <button className="text btn" type="button" onClick={ () => this.handlesignInWithTwitterClick()}>Twitter</button>
+          <button className="text btn" type="button" onClick={ () => this.handlesignInWithGoogleClick()}>Google</button>
         </div>
       </Modal>
     );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -23,7 +23,6 @@ handlerSetState(nav){
 }
 
   handleSignOutClick(){
-    this.handlerSetState('/sign-out');
      this.props.signOut();
   }
 
@@ -32,10 +31,12 @@ handlerCollapsed(stateCollapsed){
         this.setState({
           collapsed: true,
         });
+        console.log('Lo pongo true');
     }else {
       this.setState({
           collapsed: false,
         });
+      console.log('Lo pongo false');
     }
 }
 
@@ -62,7 +63,7 @@ handlerCollapsed(stateCollapsed){
               { (this.state.current === '/tutorial') ? <li className="active"><Link to="/tutorial" onClick={() => this.handlerSetState('/tutorial') }>Tutorial</Link></li> : <li><Link to="/tutorial" onClick={() => this.handlerSetState('/tutorial') }>Tutorial</Link></li> }
                 { auth.authenticated ?
                 <li className="navbar-btn"><button className="btn" type="button" onClick={ () => this.handleSignOutClick() }>Sign Out</button></li> :
-                <li><Link to="/sign-in" onClick={ ()=> this.handlerSetState('/sign-out') } { ...this.props }>Sign In</Link></li>
+                <li><Link to="/sign-in" { ...this.props }>Sign In</Link></li>
               }
             </ul>
           </div>
@@ -75,20 +76,12 @@ handlerCollapsed(stateCollapsed){
 }
 
 App.propTypes = {
-  // Injected by React RouterConfirmDialog
   children: PropTypes.node,
-  signOut: PropTypes.object.isRequired,
+  signOut: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 };
 
-function mapStateToProps(state) {
-  return {
-    auth: state.auth
-  };
-}
-
-
 export default connect(
-  state => ({
-    auth: state.auth,
-}),   Object.assign( {}, authActions))(App);
+  state => ({auth: state.auth}),
+  Object.assign( {}, authActions)
+)(App);
