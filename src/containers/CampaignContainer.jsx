1fbba39fbf34765204d43c76811745ea2b7eddf7
Davimor <davimor39@gmail.com>
2016-02-02 17:52:13 +0100

    Fix

--- src/containers/CampaignContainer.jsx
+++ src/containers/CampaignContainer.jsx
@@ -5,9 +5,17 @@ import * as campaignActions from '../actions/campaign';
 import { onAddPoint, onRemoveMissionForPoints } from '../actions/points';
 
 function mapStateToProps(state) {
+	let i = 0;
+	let id = 0;
+	while (i < state.points.length){
+		if (state.auth.id === state.points[i].id){
+			id = i;
+		}
+		i++;
+	}
   return {
     campaigns: state.campaign,
-    points: state.points[0],
+    points: state.points[id],
     users: state.points
   };
 }
