04c8d91e631e97518532324cdcea1c61a3e5f571
Davimor <davimor39@gmail.com>
2016-02-17 12:10:23 +0100

    Alertify

--- src/actions/points/actions.js
+++ src/actions/points/actions.js
@@ -16,16 +16,18 @@ export function onRemoveMissionPoints(iduser, missionnumber) {
       firebase.child(`points/${iduser}/missionpoints/${missionId}`).set(0);
     let key;
     let title;
+    let notify;
     firebase.child(`campaign`).once( 'value', snapshot =>
      { key = Object.keys( snapshot.val())[missionnumber],
       firebase.child(`campaign/${key}`).once('value', snapshot =>
-      title = snapshot.val().title
-    );
+      {
+      title = snapshot.val().title,
+      notify = {message: `Points for mission ${missionnumber + 1} : "${title}" set to 0`, status: false};
+    firebase.child(`points/${iduser}/notifications`).push(notify);
+    alertify.success(`Points for mission ${missionnumber + 1} : "${title}" set to 0`);
+    });
     });
 
-    let notify = {message: `Points for mission ${missionnumber + 1} : "${title}" set to 0`, status: false};
-    firebase.child(`points/${iduser}/notifications`).push(notify);
-    alertify.success(`Points set to 0`);
   }
     );
   };
@@ -44,13 +46,12 @@ export function onAddPoint(id, users) {
 export function onRemoveMissionForPoints(numberMission){
     return (dispatch, getState) => {
     const { firebase } = getState();
-
-    let x;
-    firebase.child('points').on('value', snapshot =>
-      x = Object.keys(snapshot.val() || []).map( id => ({id, name:snapshot.val()[id].name, missionpoints: Object.keys(snapshot.val()[id].missionpoints).map( key => ({ key, valor: snapshot.val()[id].missionpoints[key] }) ) }) )
-    );
     let i = 0;
     let idUser, idMission;
+    let x;
+    firebase.child('points').once('value', snapshot =>
+      x = Object.keys(snapshot.val() || []).map( id => ({id, name:snapshot.val()[id].name, missionpoints: Object.keys(snapshot.val()[id].missionpoints).map( key => ({ key, valor: snapshot.val()[id].missionpoints[key] }) ) }) )
+    );
     while (i < x.length){
       idUser = x[i].id;
       idMission = x[i].missionpoints[numberMission].key;
@@ -64,27 +65,26 @@ export function onBattleResult(idCampaign, points, iduser){
     return (dispatch, getState) => {
     const { firebase } = getState();
     const {auth} = getState();
-    let x;
-    firebase.child('points').on('value', snapshot =>
-      x = Object.keys(snapshot.val() || []).map( id => ({id, name:snapshot.val()[id].name, missionpoints: Object.keys(snapshot.val()[id].missionpoints).map( key => ({ key, valor: snapshot.val()[id].missionpoints[key] }) ) }) )
-    );
-    let id = x[iduser].id;
-    let idMission = x[iduser].missionpoints[idCampaign].key;
-    let missionpoints = x[iduser].missionpoints[idCampaign].valor;
+    let x, id, idMission, missionpoints, title, key, notify;
+    firebase.child('points').once('value', snapshot =>
+     {     x = Object.keys(snapshot.val() || []).map( id => ({id, name:snapshot.val()[id].name, missionpoints: Object.keys(snapshot.val()[id].missionpoints).map( key => ({ key, valor: snapshot.val()[id].missionpoints[key] }) ) }) ),
+         id = x[iduser].id;
+        idMission = x[iduser].missionpoints[idCampaign].key;
+        missionpoints = x[iduser].missionpoints[idCampaign].valor;
 
-    if ( points > missionpoints ) {
-      firebase.child(`points/${id}/missionpoints/${idMission}`).set(points);
-      let key;
-      firebase.child(`campaign`).once( 'value', snapshot =>
-        key = Object.keys( snapshot.val())[idCampaign]
-      );
-      let title;
-      firebase.child(`campaign/${key}`).once('value', snapshot =>
-        title = snapshot.val().title
-      );
-      let notify = {message: `Points for mission ${parseInt(idCampaign) + 1 }: "${title}" updated with ${points} points`, status: false};
-     firebase.child(`points/${auth.id}/notifications`).push(notify);
-    }
+        if ( points > missionpoints ) {
+            firebase.child(`points/${id}/missionpoints/${idMission}`).set(points);
+            firebase.child(`campaign`).once( 'value', snapshot =>
+              key = Object.keys( snapshot.val())[idCampaign]
+            );
+            firebase.child(`campaign/${key}`).once('value', snapshot =>
+              title = snapshot.val().title
+            );
+            notify = {message: `Points for mission ${parseInt(idCampaign) + 1 }: "${title}" updated with ${points} points`, status: false};
+           firebase.child(`points/${auth.id}/notifications`).push(notify);
+           alertify.success(`Points for mission ${parseInt(idCampaign) + 1 }: "${title}" updated with ${points} points`);
+        }
+   } );
 
     };
 }
